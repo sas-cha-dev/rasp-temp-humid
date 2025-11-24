9b4d9074c6c82240e8dae12b85abb0379cfec1a9
Sascha Schweitzer <sascha.schweitzer@posteo.de>
2025-11-24 18:49:12 +0100

    chore: removed unused powershell script

--- Backend/load-env.ps1
+++
@@ -1,9 +0,0 @@
-# load-env.ps1
-Get-Content .env | ForEach-Object {
-    if ($_ -match '^\s*([^#][^=]*?)\s*=\s*(.+?)\s*$') {
-        $name = $matches[1]
-        $value = $matches[2]
-        Set-Item -Path "env:$name" -Value $value
-        Write-Host "Loaded: $name=$value"
-    }
-}
